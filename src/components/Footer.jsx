6320ecc58b26630a50b7a1a4cef8c304a1903782
HAMZA BENHAMMOU <ham.benhammou@gmail.com>
2025-04-27 10:58:45 +0100

    change linkedin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
             <div className="footer-container">
                 <p>&copy; {new Date().getFullYear()} {profileData.name}. Tous droits réservés.</p>
                 <div className="social-links">
-                    <a href="www.linkedin.com/in/h-benhammou" className="social-link">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/h-benhammou/" className="social-link">LinkedIn</a>
                     <a href="https://github.com/H-Benhammou" className="social-link">GitHub</a>
                     <a href={`mailto:${profileData.email}`} className="social-link">Email</a>
                 </div>
